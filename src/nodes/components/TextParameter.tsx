8180f04d370f1fb38c011264b30d791919f936b3
fabrisdev <ezepuntocom@hotmail.com>
2025-08-04 21:42:20 -0300

    fixed textarea being able to be to small

--- src/nodes/components/TextParameter.tsx
+++ src/nodes/components/TextParameter.tsx
@@ -16,7 +16,7 @@ export function TextParameter({ id, data }: ParameterProps) {
 				id={inputId}
 				name="text"
 				value={text}
-				className="nodrag resize border-2 border-[#3c3c3c] rounded-sm p-2"
+				className="nodrag resize border-2 border-[#3c3c3c] rounded-sm p-2 min-w-full"
 				placeholder="Text to display"
 				onChange={(e) => updateNodeData(id, { text: e.target.value })}
 			/>
