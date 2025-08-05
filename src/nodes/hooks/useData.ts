8987c11e9b3609e58733d0c768cd5177e1df5e8a
fabrisdev <ezepuntocom@hotmail.com>
2025-08-05 06:02:15 -0300

    removed left debug message

--- src/nodes/hooks/useData.ts
+++ src/nodes/hooks/useData.ts
@@ -7,7 +7,5 @@ type Props = {
 
 export function useData({ id, prop }: Props): any {
 	const nodes = useNodes((state) => state.nodes);
-	const a = nodes.find((node) => node.id === id);
-	console.log(a);
-	return a?.data[prop];
+	return nodes.find((node) => node.id === id)?.data[prop];
 }
