c99b8384540710e9effeb9cc26152b40c208bb35
fabrisdev <ezepuntocom@hotmail.com>
2025-08-06 16:56:15 -0300

    Fixed only being able to click on text

--- src/file-manager/components/File.tsx
+++ src/file-manager/components/File.tsx
@@ -6,20 +6,22 @@ import { useNodes } from "@/nodes/store/store";
 export function File({ children }: { children: React.ReactNode }) {
 	const { deleteFile } = useNodes((state) => state.actions);
 	return (
-		<Node className="hover:bg-[#3c3c3c] cursor-pointer flex justify-between items-center">
-			<Link href={`/file/${children}`}>{children}</Link>
-			<button
-				type="button"
-				className="border-2 border-[#3c3c3c] p-2 rounded-sm hover:bg-red-400 cursor-pointer"
-				onClick={() => deleteFile(children as string)}
-			>
-				<Image
-					src={"/close.png"}
-					alt={`Delete ${children} file`}
-					width={20}
-					height={20}
-				/>
-			</button>
-		</Node>
+		<Link href={`/file/${children}`}>
+			<Node className="hover:bg-[#3c3c3c] cursor-pointer flex justify-between items-center">
+				<p>{children}</p>
+				<button
+					type="button"
+					className="border-2 border-[#3c3c3c] p-2 rounded-sm hover:bg-red-400 cursor-pointer"
+					onClick={() => deleteFile(children as string)}
+				>
+					<Image
+						src={"/close.png"}
+						alt={`Delete ${children} file`}
+						width={20}
+						height={20}
+					/>
+				</button>
+			</Node>
+		</Link>
 	);
 }
