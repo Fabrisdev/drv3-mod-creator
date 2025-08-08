94662c5433f37db10c6ff8c50ad66497cbf93505
fabrisdev <ezepuntocom@hotmail.com>
2025-08-08 05:50:43 -0300

    Adding nodes working

--- src/context-menu/options/SpawnOption.tsx
+++ src/context-menu/options/SpawnOption.tsx
@@ -1,4 +1,8 @@
+import { useParams } from "next/navigation";
+import { useState } from "react";
 import { nodeNames } from "@/nodes/node-names";
+import { useNodes } from "@/nodes/store/store";
+import type { NodeNameTypes } from "@/nodes/types";
 import { Button } from "@/ui/Button";
 import { Container } from "@/ui/Container";
 import { ContainerTitle } from "@/ui/ContainerTitle";
@@ -9,16 +13,36 @@ type Props = {
 };
 
 export function SpawnOption({ closeMenu }: Props) {
+	const { addNode } = useNodes((state) => state.actions);
+	const [nodeSelected, setNodeSelected] = useState<NodeNameTypes>("start");
+	const { filename } = useParams();
+
 	const nodeOptions = Object.entries(nodeNames).map(([type, name]) => (
-		<option key={type}>{name}</option>
+		<option key={type} value={type}>
+			{name}
+		</option>
 	));
+
 	return (
 		<Container>
 			<ContainerTitle>Spawn node...</ContainerTitle>
 			<div className="flex gap-2">
-				<Select>{nodeOptions}</Select>
+				<Select
+					onChange={(selected) => setNodeSelected(selected as NodeNameTypes)}
+					value={nodeSelected}
+				>
+					{nodeOptions}
+				</Select>
 				<Button
-					onClick={() => {
+					onClick={(e) => {
+						addNode(
+							nodeSelected,
+							{
+								x: e.clientX,
+								y: e.clientY,
+							},
+							filename as string,
+						);
 						closeMenu();
 					}}
 				>
