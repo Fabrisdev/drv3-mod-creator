416a773e4b66bd0446de349a12178d5f8cf952aa
fabrisdev <ezepuntocom@hotmail.com>
2025-08-08 03:06:21 -0300

    Added file parameter

---
+++ src/nodes/components/FileParameter.tsx
@@ -0,0 +1,36 @@
+import { useParams } from "next/navigation";
+import { useShallow } from "zustand/shallow";
+import { useData } from "../hooks/useData";
+import { useNodes } from "../store/store";
+import type { ParameterProps } from "../types";
+
+type Props = {
+	hideCurrentFile?: boolean;
+} & ParameterProps;
+
+export function FileParameter({ id, hideCurrentFile }: Props) {
+	const { filename } = useParams();
+	const { updateNodeData } = useNodes((store) => store.actions);
+	const files = useNodes(useShallow((state) => Object.keys(state.files)));
+	const filteredFiles = hideCurrentFile
+		? files.filter((file) => file !== filename)
+		: files;
+	const text = useData<string>({ id, prop: "text" });
+	const filesMapped = filteredFiles.map((file) => (
+		<option value={file} key={file}>
+			{file}
+		</option>
+	));
+
+	return (
+		<select
+			value={text}
+			className="nodrag resize border-2 border-[#3c3c3c] rounded-sm p-2 min-w-full"
+			onChange={(e) =>
+				updateNodeData(id, { text: e.target.value }, filename as string)
+			}
+		>
+			{filesMapped}
+		</select>
+	);
+}
