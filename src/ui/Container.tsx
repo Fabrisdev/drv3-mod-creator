6303c7dd35cd9caaa9ffc60b539b680de845aec2
fabrisdev <ezepuntocom@hotmail.com>
2025-08-08 04:44:26 -0300

    Added new props for Container

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -1,17 +1,23 @@
-import type { CSSProperties, PropsWithChildren } from "react";
+import type { CSSProperties, PropsWithChildren, Ref } from "react";
 
 type Props = {
 	className?: string;
 	style?: CSSProperties;
+	id?: string;
+	ref?: Ref<HTMLDivElement> | null;
 };
 
 export function Container({
 	className,
 	children,
 	style,
+	id,
+	ref,
 }: PropsWithChildren<Props>) {
 	return (
 		<div
+			id={id}
+			ref={ref}
 			className={`${className} p-2 rounded-sm bg-[#1e1e1e] border-2 border-[#3c3c3c]`}
 			style={style}
 		>
