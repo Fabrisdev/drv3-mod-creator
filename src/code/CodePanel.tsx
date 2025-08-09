82976a09e6cb8544650f75c234213f05fdba6aa6
fabrisdev <ezepuntocom@hotmail.com>
2025-08-09 02:37:31 -0300

    Fixed generated code lines being off

--- src/code/CodePanel.tsx
+++ src/code/CodePanel.tsx
@@ -15,7 +15,7 @@ export function CodePanel() {
 	if (!visible) return null;
 	return (
 		<Panel position="top-right">
-			<Container className="whitespace-pre-line opacity-30 hover:opacity-100 duration-150 max-h-100 overflow-y-scroll max-w-90 text-sm">
+			<Container className="whitespace-pre-line opacity-30 hover:opacity-100 duration-150 max-h-100 overflow-y-scroll text-sm">
 				<p className="text-center">Code generated</p>
 				<CodeEditor code={code} disabled />
 			</Container>
