3133b206e31b0c5d451be6eb7d283491c23ab559
fabrisdev <ezepuntocom@hotmail.com>
2025-08-09 00:25:13 -0300

    removed unnecessary styles and used container instead of node

--- src/code/CodePanel.tsx
+++ src/code/CodePanel.tsx
@@ -1,7 +1,7 @@
 import { Panel } from "@xyflow/react";
 import { useEffect, useState } from "react";
 import Editor from "react-simple-code-editor";
-import { Node } from "@/nodes/components/Node";
+import { Container } from "@/ui/Container";
 import { highlight } from "./highlighter";
 import { useCode } from "./hooks/useCode";
 
@@ -16,16 +16,15 @@ export function CodePanel() {
 	if (!visible) return null;
 	return (
 		<Panel position="top-right">
-			<Node className="whitespace-pre-line opacity-30 hover:opacity-100 duration-150 max-h-100 overflow-y-scroll max-w-90 text-sm">
+			<Container className="whitespace-pre-line opacity-30 hover:opacity-100 duration-150 max-h-100 overflow-y-scroll max-w-90 text-sm">
 				<p className="text-center">Code generated</p>
 				<Editor
 					value={code}
 					highlight={highlight}
-					className="whitespace-pre-line opacity-30 hover:opacity-100 duration-150 max-h-100 overflow-y-scroll max-w-90 text-sm"
 					disabled
 					onValueChange={() => {}}
 				/>
-			</Node>
+			</Container>
 		</Panel>
 	);
 }
