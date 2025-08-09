085e48dac4b2aa95c23dcfa870f8401658fc9207
fabrisdev <ezepuntocom@hotmail.com>
2025-08-09 00:22:22 -0300

    Added highlighting to text generated

--- src/code/CodePanel.tsx
+++ src/code/CodePanel.tsx
@@ -1,6 +1,8 @@
 import { Panel } from "@xyflow/react";
 import { useEffect, useState } from "react";
+import Editor from "react-simple-code-editor";
 import { Node } from "@/nodes/components/Node";
+import { highlight } from "./highlighter";
 import { useCode } from "./hooks/useCode";
 
 export function CodePanel() {
@@ -16,7 +18,13 @@ export function CodePanel() {
 		<Panel position="top-right">
 			<Node className="whitespace-pre-line opacity-30 hover:opacity-100 duration-150 max-h-100 overflow-y-scroll max-w-90 text-sm">
 				<p className="text-center">Code generated</p>
-				<p>{code}</p>
+				<Editor
+					value={code}
+					highlight={highlight}
+					className="whitespace-pre-line opacity-30 hover:opacity-100 duration-150 max-h-100 overflow-y-scroll max-w-90 text-sm"
+					disabled
+					onValueChange={() => {}}
+				/>
 			</Node>
 		</Panel>
 	);
