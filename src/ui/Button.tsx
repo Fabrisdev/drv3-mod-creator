dcf85c724abeb65e7194c7706ccc2984a8662ee3
fabrisdev <ezepuntocom@hotmail.com>
2025-08-08 22:42:36 -0300

    Removed unnecesarry brackets

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -8,7 +8,7 @@ export function Button({ onClick, children }: PropsWithChildren<Props>) {
 	return (
 		<button
 			type="button"
-			className={`p-2 rounded-sm bg-[#1e1e1e] border-2 border-[#3c3c3c] hover:bg-[#3c3c3c] cursor-pointer`}
+			className="p-2 rounded-sm bg-[#1e1e1e] border-2 border-[#3c3c3c] hover:bg-[#3c3c3c] cursor-pointer"
 			onClick={(e) => onClick(e)}
 		>
 			{children}
