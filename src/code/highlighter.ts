d9207e7fbc8633312586747636b94648a04a7ae8
fabrisdev <ezepuntocom@hotmail.com>
2025-08-08 23:48:27 -0300

    Added new colors

--- src/code/highlighter.ts
+++ src/code/highlighter.ts
@@ -2,13 +2,41 @@ export function highlight(text: string) {
 	const code = text
 		.split("\n")
 		.map((line) => {
-			const lineSplitted = line.split("//");
-			if (lineSplitted.length === 1) return escapeHtml(line);
-			const lineOnlyWithComments = lineSplitted.slice(1).join("//");
-			return `${escapeHtml(lineSplitted[0])}<span style="color: green">${escapeHtml(`//${lineOnlyWithComments}`)}</span>`;
+			const html = escapeHtml(line);
+			const coloredComments = colorComments(html);
+			const coloredBrackets = colorBrackets(coloredComments);
+			const coloredKeywords = colorKeywords(coloredBrackets);
+			const coloredEquals = colorEquals(coloredKeywords);
+			return coloredEquals;
 		})
 		.join("<br>");
 	return code;
+}
+
+function colorEquals(html: string) {
+	return html.replace(
+		/(&nbsp;=&nbsp;)/g,
+		'<span style="color: #7982D6">$1</span>',
+	);
+}
+
+function colorKeywords(html: string) {
+	const color = "#79AAD6";
+	const keywords = ["LAB", "WAK", "FDS", "FLG"];
+	const regex = new RegExp(`\\b(${keywords.join("|")})\\b`, "g");
+	return html.replace(regex, `<span style="color: ${color}">$1</span>`);
+}
+
+function colorComments(html: string) {
+	const color = "green";
+	return html.replace(/(\/\/.*)/g, `<span style="color: ${color}">$1</span>`);
+}
+
+function colorBrackets(html: string) {
+	const color = "#D6B279";
+	html = html.replace(/(&lt;)/g, `<span style="color: ${color}">$1</span>`);
+	html = html.replace(/(&gt;)/g, `<span style="color: ${color}">$1</span>`);
+	return html;
 }
 
 function escapeHtml(text: string): string {
