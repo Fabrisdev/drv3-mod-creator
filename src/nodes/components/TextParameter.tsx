92d384a3a98e57bb3435575f290d32862f226831
fabrisdev <ezepuntocom@hotmail.com>
2025-08-10 01:47:07 -0300

    removed unnecessary stuff

--- src/nodes/components/TextParameter.tsx
+++ src/nodes/components/TextParameter.tsx
@@ -2,36 +2,33 @@ import { useParams } from "next/navigation";
 import { type PropsWithChildren, useId } from "react";
 import { useData } from "../hooks/useData";
 import { useNodes } from "../store/store";
-import type { ParameterProps } from "../types";
 
 type Props = {
-	placeholder?: string;
-} & ParameterProps;
+	id: string;
+};
 
-export function TextParameter({
-	id,
-	children,
-	placeholder,
-}: PropsWithChildren<Props>) {
+export function TextParameter({ id }: PropsWithChildren<Props>) {
 	const { filename } = useParams();
 	const inputId = useId();
 	const { updateNodeData } = useNodes((store) => store.actions);
 	const text = useData({ id, prop: "text" });
 
+	function handleChange(newText: string) {
+		updateNodeData(id, { text: newText }, filename as string);
+	}
+
 	return (
 		<div className="flex flex-col">
 			<label htmlFor={inputId} className="text-center w-full">
-				{children}
+				Text
 			</label>
 			<textarea
 				id={inputId}
 				name="text"
 				value={text}
 				className="nodrag resize border-2 border-[#3c3c3c] rounded-sm p-2 min-w-full"
-				placeholder={placeholder ? placeholder : "Text to display"}
-				onChange={(e) =>
-					updateNodeData(id, { text: e.target.value }, filename as string)
-				}
+				placeholder="Text to display"
+				onChange={(e) => handleChange(e.target.value)}
 			/>
 		</div>
 	);
