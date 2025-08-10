f0e9b41543c5b86eea90b36213018dd01da6f25c
fabrisdev <ezepuntocom@hotmail.com>
2025-08-10 01:47:13 -0300

    Added new parameters

--- src/nodes/TextNode.tsx
+++ src/nodes/TextNode.tsx
@@ -1,16 +1,33 @@
 import { Handle, type NodeProps, Position } from "@xyflow/react";
+import { useState } from "react";
 import { CharacterParameter } from "./components/CharacterParameter";
 import { Node } from "./components/Node";
+import {
+	type TextMode,
+	TextModeParameter,
+} from "./components/TextModeParameter";
+import {
+	type OutputMode,
+	TextOutputModeParameter,
+} from "./components/TextOutputModeParameter";
 import { TextParameter } from "./components/TextParameter";
 
 export function TextNode({ id, data }: NodeProps) {
+	const [textMode, setTextMode] = useState<TextMode>("normal");
+	const [outputMode, setOutputMode] = useState<OutputMode>("colored");
 	return (
 		<Node>
 			<div className="flex flex-col gap-2">
 				<CharacterParameter id={id} data={data} />
-				<TextParameter id={id} data={data}>
-					Text
-				</TextParameter>
+				<TextModeParameter
+					mode={textMode}
+					handleChange={(m) => setTextMode(m)}
+				/>
+				<TextOutputModeParameter
+					mode={outputMode}
+					handleChange={(m) => setOutputMode(m)}
+				/>
+				<TextParameter id={id} />
 			</div>
 			<Handle type="target" position={Position.Left} />
 			<Handle type="source" position={Position.Right} />
