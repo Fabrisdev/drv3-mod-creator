301bb90e036732736e679f8a56b9ee05cce17c3d
fabrisdev <ezepuntocom@hotmail.com>
2025-08-10 02:04:17 -0300

    Simplified text mode parameter

--- src/nodes/components/TextModeParameter.tsx
+++ src/nodes/components/TextModeParameter.tsx
@@ -1,31 +1,18 @@
+import { Button } from "@/ui/Button";
+
 export type TextMode = "normal" | "thinking";
 
 type Props = {
-	mode: TextMode;
 	handleChange: (mode: TextMode) => void;
 };
 
-export function TextModeParameter({ mode, handleChange }: Props) {
+export function TextModeParameter({ handleChange }: Props) {
 	return (
 		<div className="flex items-center justify-between">
 			<p>Text mode</p>
 			<div className="bg-[#3c3c3c] rounded-sm flex gap-2 w-fit p-1">
-				<label className="flex gap-1">
-					<input
-						type="radio"
-						checked={mode === "normal"}
-						onChange={() => handleChange("normal")}
-					/>
-					Normal
-				</label>
-				<label className="flex gap-1">
-					<input
-						type="radio"
-						checked={mode === "thinking"}
-						onChange={() => handleChange("thinking")}
-					/>
-					<p>Thinking</p>
-				</label>
+				<Button onClick={() => handleChange("normal")}>Normal</Button>
+				<Button onClick={() => handleChange("thinking")}>Thinking</Button>
 			</div>
 		</div>
 	);
