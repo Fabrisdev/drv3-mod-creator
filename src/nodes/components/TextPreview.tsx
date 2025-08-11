eba2e4afef8c69742f74878ec85c61abe630975f
fabrisdev <ezepuntocom@hotmail.com>
2025-08-10 21:21:31 -0300

    Previous character working

--- src/nodes/components/TextPreview.tsx
+++ src/nodes/components/TextPreview.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from "react";
+import { useCode } from "@/code/hooks/useCode";
 import { PreviewRenderer } from "./PreviewRenderer";
 
 type Props = {
@@ -7,7 +8,28 @@ type Props = {
 };
 
 export function TextPreview({ character, text }: PropsWithChildren<Props>) {
+	const { code } = useCode();
+
+	function extractPreviousCharacter() {
+		const regex = /<CHN\s+([^>]+)>/g;
+
+		const matches = [...code.matchAll(regex)];
+		if (matches.length === 0) {
+			return;
+		} else {
+			const lastChn = matches[matches.length - 1][1];
+			return lastChn.slice(1, 4);
+		}
+	}
+
 	const characterNameImage = (() => {
+		console.log(character);
+
+		if (character === "unset" || character === "") {
+			const previousCharacter = extractPreviousCharacter();
+			if (previousCharacter === undefined) return "";
+			return `chara_name_${previousCharacter}_US`;
+		}
 		if (character === "chara_Blank") return "";
 		if (character.startsWith("C"))
 			return `chara_name_${character.slice(1, 4)}_US`;
