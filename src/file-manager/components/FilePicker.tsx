229af4a53cef9a31c61029ff0a1d0e03d416f44a
fabrisdev <ezepuntocom@hotmail.com>
2025-08-19 17:58:07 -0300

    Made clearer when you dont have any other files than your current one

--- src/file-manager/components/FilePicker.tsx
+++ src/file-manager/components/FilePicker.tsx
@@ -2,9 +2,9 @@
 
 import { useShallow } from "zustand/shallow";
 import { useFilename } from "@/file-manager/hooks/useFilename";
-import { Node } from "@/nodes/components/Node";
 import { useNodes } from "@/nodes/store/store";
-import { CloseIcon } from "../icons/CloseIcon";
+import { Container } from "@/ui/Container";
+import { CloseButton } from "./CloseButton";
 import { CreateFile } from "./CreateFile";
 import { CreateScene } from "./CreateScene";
 import { File } from "./File";
@@ -16,36 +16,33 @@ type Props = {
 
 export function FilePicker({ className, onAskToClose }: Props) {
 	const { filename } = useFilename();
-	const filenames = useNodes(
-		useShallow((state) => Object.keys(state.files)),
-	).filter((file) => file !== filename);
+	const filenames = useNodes(useShallow((state) => Object.keys(state.files)));
+	const filenamesWithoutCurrentOne = filenames.filter(
+		(file) => file !== filename,
+	);
 
-	const filenamesMapped = filenames
+	const filenamesMapped = filenamesWithoutCurrentOne
 		.map((filename) => filename.slice(1))
 		.map((filename) => <File key={filename}>{filename}</File>);
 
 	return (
-		<Node className={`min-w-100 flex flex-col gap-2 ${className}`}>
+		<Container className={`min-w-100 flex flex-col gap-2 ${className}`}>
 			<div className="flex justify-between items-center">
 				<p>
-					{filenames.length !== 0
+					{filenamesWithoutCurrentOne.length !== 0
 						? "Pick a file"
-						: "You don't have any files yet. How about creating one?"}
+						: filenames.length !== 0
+							? "You don't have any other files other than your current one. How about creating one?"
+							: "You don't have any files yet. How about creating one?"}
 				</p>
-				{onAskToClose && (
-					<button
-						type="button"
-						onClick={onAskToClose}
-						className="cursor-pointer"
-					>
-						<CloseIcon alt="Close file picker menu" size={15} />
-					</button>
-				)}
+				<CloseButton onAskToClose={onAskToClose} />
 			</div>
 			{filenamesMapped}
-			{filenames.length !== 0 && "Or..."}
+			{filenames.length !== 0 &&
+				filenamesWithoutCurrentOne.length !== 0 &&
+				"Or..."}
 			<CreateFile />
 			<CreateScene />
-		</Node>
+		</Container>
 	);
 }
