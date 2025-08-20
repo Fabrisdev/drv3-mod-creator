bc0ec5640dfd5c4d8faa9b3e39caddd74e24d56f
fabrisdev <ezepuntocom@hotmail.com>
2025-08-20 14:29:09 -0300

    Added handles

--- src/nodes/SetLifeInFile.tsx
+++ src/nodes/SetLifeInFile.tsx
@@ -1,3 +1,4 @@
+import { Handle, Position } from "@xyflow/react";
 import { LifeInFileParameter } from "./components/LifeInFileParameter";
 import { Node } from "./components/Node";
 
@@ -6,6 +7,8 @@ export function SetLifeInFile() {
 		<Node>
 			<p>Type of life in file</p>
 			<LifeInFileParameter />
+			<Handle type="target" position={Position.Left} />
+			<Handle type="source" position={Position.Right} />
 		</Node>
 	);
 }
