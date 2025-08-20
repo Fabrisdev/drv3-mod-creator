441a469c794d9f6847feb059d9b9640d4b4daccf
fabrisdev <ezepuntocom@hotmail.com>
2025-08-20 15:34:30 -0300

    Added parameters to node

--- src/nodes/FlgNode.tsx
+++ src/nodes/FlgNode.tsx
@@ -1,10 +1,14 @@
-import { Handle, Position } from "@xyflow/react";
+import { Handle, type NodeProps, Position } from "@xyflow/react";
+import { BoolParameter } from "./components/BoolParameter";
+import { FlgParameter } from "./components/FlgParameter";
 import { Node } from "./components/Node";
 
-export function FlgNode() {
+export function FlgNode({ id }: NodeProps) {
 	return (
 		<Node>
 			<p>Turn flag on/off</p>
+			<FlgParameter />
+			<BoolParameter id={id} />
 			<Handle type="target" position={Position.Left} />
 			<Handle type="source" position={Position.Right} />
 		</Node>
---
+++ src/nodes/components/FlgParameter.tsx
@@ -0,0 +1,5 @@
+import { Input } from "@/ui/Input";
+
+export function FlgParameter() {
+	return <Input placeholder="Flag" />;
+}
