2ecffab88fbf4d381aa33b3e58249ec44cd158c2
fabrisdev <ezepuntocom@hotmail.com>
2025-08-20 14:27:54 -0300

    Added it to the store

--- src/panel/NodesPanel.tsx
+++ src/panel/NodesPanel.tsx
@@ -45,6 +45,7 @@ export function NodesPanel() {
 					<NodeOption type="set_time">Add Set time node</NodeOption>
 					<NodeOption type="set_chapter">Add Set chapter node</NodeOption>
 					<NodeOption type="set_dead">Add Set dead node</NodeOption>
+					<NodeOption type="life_in_file">Add Set life in file node</NodeOption>
 					<NodeOption type="end">Add End node</NodeOption>
 				</Container>
 				<Container className="flex flex-col gap-2">
