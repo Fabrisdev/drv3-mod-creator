f049f01a4e61a4c1c0b3cc390a08fff4eec6a6f5
fabrisdev <ezepuntocom@hotmail.com>
2025-08-20 22:42:20 -0300

    updated various files

--- src/nodes/components/SwitchCases.tsx
+++ src/nodes/components/SwitchCases.tsx
@@ -1,6 +1,6 @@
 import { useFilename } from "@/file-manager/hooks/useFilename";
 import { useData } from "../hooks/useData";
-import { useNodes } from "../store/file";
+import { createFileStore } from "../store/file";
 import { SwitchCase } from "./SwitchCase";
 
 type Props = {
@@ -9,17 +9,17 @@ type Props = {
 
 export function SwitchCases({ id }: Props) {
 	const cases = useData({ id, prop: "cases" }) ?? [];
-	const { updateCase, removeCase } = useNodes((state) => state.actions);
 	const { filename } = useFilename();
+	const useFileStore = createFileStore(filename);
+
+	const { updateCase, removeCase } = useFileStore((state) => state.actions);
 	return cases.map((c) => (
 		<SwitchCase
 			id={c.id}
 			key={c.id}
 			value={c.value}
-			onChange={(newValue) =>
-				updateCase(id, filename as string, c.id, newValue)
-			}
-			onDelete={() => removeCase(id, filename as string, c.id)}
+			onChange={(newValue) => updateCase(id, c.id, newValue)}
+			onDelete={() => removeCase(id, c.id)}
 		/>
 	));
 }
